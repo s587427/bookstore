b7885b43c3c7f88c9d3b0d63f93f0db83a9fa6a9
s587427 <s587427@gmail.com>
2022-12-01 11:39:15 +0800

    book css
    book css

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -1,4 +1,5 @@
-// 註冊插件
+
+
 FilePond.registerPlugin(
     FilePondPluginFileEncode,
     FilePondPluginImagePreview,
@@ -7,7 +8,6 @@ FilePond.registerPlugin(
 
 // 樣式修改
 FilePond.setOptions({
-    stylePanelLayout: 'compact',
     stylePanelAspectRatio: '150:100',
     imageResizeTargetWidth: 100,
     imageResizeTargetHeight: 150,
@@ -16,4 +16,8 @@ FilePond.setOptions({
 // parse方法讓我們自動在頁面上加載 FilePond 元素
 // FilePond 在<body>元素中找碴子樹中具有該類.filepond
 FilePond.parse(document.body);
+
+
+
+
 
